db5a6c0146acb324c797700ba1c74cbe14a7fb20
forgitd <1826671475@qq.com>
2021-07-13 12:19:00 +0800

    项目结构

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,20 @@
 import { createStore } from 'vuex'
 
+import actions from "./actions";
+import mutations from "./mutations";
+import getters from "./getters";
+import moduleA from "./modules/moduleA";
+
+
+const a = moduleA
+
 export default createStore({
   state: {
   },
-  mutations: {
-  },
-  actions: {
-  },
+  mutations,
+  actions,
+  getters,
   modules: {
+    a
   }
 })
---
+++ src/views/main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>mainYemian</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "main"
+}
+</script>
+
+<style scoped>
+  @import "../assets/css/normalize.css";
+</style>
